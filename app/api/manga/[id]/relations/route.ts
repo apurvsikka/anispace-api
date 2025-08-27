1bf172acb008da8e9f11a9910323fb66b283c6fd
apurv <apurvsikka@gmail.com>
2025-08-27 19:49:05 +0530

    feat: 🎐add manda relations to the api 🎐

---
+++ app/api/manga/[id]/relations/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server"
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const id = parseInt(params.id, 10) // ensure integer
+
+    const ALQUERY = `
+      query Media($mediaId: Int) {
+        Media(id: $mediaId) {
+          relations {
+            edges {
+              relationType
+              node {
+                id
+                type
+                title {
+                  romaji
+                  english
+                  native
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+
+    const response = await fetch("https://graphql.anilist.co", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        query: ALQUERY,
+        variables: { mediaId: id },
+      }),
+      cache: "no-store",
+    })
+
+    const data = await response.json()
+    const d = await data.data.Media.relations.edges
+    return NextResponse.json(d)
+  } catch (error) {
+    console.error("Error in GET /api/manga/info/[id]:", error)
+    return NextResponse.json(
+      { error: "Failed to fetch manga info" },
+      { status: 500 }
+    )
+  }
+}
